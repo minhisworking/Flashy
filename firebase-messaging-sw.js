1411824a4e98ee1baa610f5c864a20da24fc3ede
minhisworking <minhisworking@gmail.com>
2026-09-15 00:17:02 +0700

    hope

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,32 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+importScripts(
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js'
+);
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyCiaMLU3oRRJRvXWV6wzOOOyT9R5BtEwFI",
   authDomain: "flashyapp-45c1a.firebaseapp.com",
   projectId: "flashyapp-45c1a",
   storageBucket: "flashyapp-45c1a.firebasestorage.app",
   messagingSenderId: "775809731068",
-  appId: "1:775809731068:web:02fada2a2150ca0186ca79",
-  measurementId: "G-TJNC4H01W0"
-};
+  appId: "1:775809731068:web:02fada2a2150ca0186ca79"
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Background message:', payload);
+
+  const notificationTitle =
+    payload.notification?.title || 'Flashy';
+
+  const notificationOptions = {
+    body: payload.notification?.body || '',
+    icon: '/Flashy/icon-192.png'
+  };
+
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});

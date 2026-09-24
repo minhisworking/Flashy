5ef7a4d49f4fae7f692e2e17a94e130b2e92825b
minhisworking <minhisworking@gmail.com>
2026-09-25 05:10:56 +0700

    phát cuối

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,3 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-// Firebase config (giữ nguyên config cũ của bạn)
-firebase.initializeApp({
-  apiKey: "AIzaSyCiaMLU3oRRJRvXWV6wzOOOyT9R5BtEwFI",
-  authDomain: "flashyapp-45c1a.firebaseapp.com",
-  projectId: "flashyapp-45c1a",
-  storageBucket: "flashyapp-45c1a.firebasestorage.app",
-  messagingSenderId: "775809731068",
-  appId: "1:775809731068:web:02fada2a2150ca0186ca79",
-  measurementId: "G-TJNC4H01W0"
-});
-
-const messaging = firebase.messaging();
-
 
 // Thêm đoạn này vào file firebase-messaging-sw.js
 self.addEventListener('notificationclick', function(event) {
@@ -37,3 +20,26 @@ self.addEventListener('notificationclick', function(event) {
         })
     );
 });
+
+
+
+
+
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+// Firebase config (giữ nguyên config cũ của bạn)
+firebase.initializeApp({
+  apiKey: "AIzaSyCiaMLU3oRRJRvXWV6wzOOOyT9R5BtEwFI",
+  authDomain: "flashyapp-45c1a.firebaseapp.com",
+  projectId: "flashyapp-45c1a",
+  storageBucket: "flashyapp-45c1a.firebasestorage.app",
+  messagingSenderId: "775809731068",
+  appId: "1:775809731068:web:02fada2a2150ca0186ca79",
+  measurementId: "G-TJNC4H01W0"
+});
+
+const messaging = firebase.messaging();
+
+
